942a07d4c0c34990fb5534fb5c9585255b0885c7
gabriel <gabrielssc.ti@gmail.com>
2022-01-22 21:21:29 -0300

    Gets attribute data from csv files dev#17

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController player1;
+    [SerializeField]
+    private PlayerController player2;
+
+    private Dictionary<int, Attributes> dayAttributes;
+    private Dictionary<int, Attributes> monthAttributes;
+    private Dictionary<int, Attributes> yearAttributes;
+    // Start is called before the first frame update
+
+    private void LoadAttributes(){
+        dayAttributes =   FileDataManager.GetAttributes(FileDataManager.DAY);
+        monthAttributes = FileDataManager.GetAttributes(FileDataManager.MONTH);
+        yearAttributes  = FileDataManager.GetAttributes(FileDataManager.YEAR);
+    }
+
+    void Start()
+    {
+        LoadAttributes();
+        Attributes attributes;
+        dayAttributes.TryGetValue(10, out attributes);
+        Debug.Log(attributes.ToString());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FileDataManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+public class FileDataManager
+{
+    public const string DAY   = "Dia.csv";
+    public const string MONTH = "Mes.csv";
+    public const string YEAR  = "Ano.csv";
+
+    public static Dictionary<int, Attributes> GetAttributes(string time){
+        if ((time != DAY) && (time != MONTH) && (time != YEAR)){
+            Debug.Log("Invalid String!");
+            return null;
+        }
+
+        var res = new Dictionary<int, Attributes>();
+        using (var reader = new StreamReader("Assets/Data/" + time)){
+            while(!reader.EndOfStream){
+                var values        = reader.ReadLine().Split(',');
+                var days          = values[0];
+                var advantageName = values[1];
+                var attributes    = values[2];
+                Debug.Log(days + " | " + advantageName + " | " + attributes);
+                
+                var attributesArray = attributes.Split('|');
+                var attributeObj    = new Attributes(advantageName, int.Parse(attributesArray[0]), int.Parse(attributesArray[1]), int.Parse(attributesArray[2]), int.Parse(attributesArray[3]));
+                
+                foreach (string day in days.Split('-')){
+                    res.Add(int.Parse(day), attributeObj);
+                }
+            }
+        }
+        return res;
+    }
+}
