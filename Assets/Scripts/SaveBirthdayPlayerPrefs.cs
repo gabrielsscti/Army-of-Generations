c292000ac80557064a7222aa015236faaa02f59a
Nuninho Neto <nuninho@opsgamestudio.com>
2022-01-23 17:47:28 -0300

    Menu

---
+++ Assets/Scripts/SaveBirthdayPlayerPrefs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveBirthdayPlayerPrefs : MonoBehaviour
+{
+    public Dropdown dropdownDay;
+    public Dropdown dropdownMonth;
+    public Dropdown dropdownYear;
+    public int playerId = 1;
+
+    public void Execute()
+    {
+        PlayerPrefs.SetInt("dia" + playerId, dropdownDay.value + 1);
+        PlayerPrefs.SetInt("mes" + playerId, dropdownMonth.value + 1);
+        PlayerPrefs.SetInt("ano" + playerId, dropdownYear.value + 1984);
+    }
+}
